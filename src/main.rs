dde43a82cb8fd76060f28c72a698addd700c5892
Roman Danilov <neci.03@mail.ru>
2023-11-25 06:26:02 +0500

    Improve Error Messages

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ mod structurs;
 extern crate crypto;
 extern crate rand;
 
-use std::fs::{copy, File};
-use std::io::{BufReader, Read, stdout, Write};
+use std::fs::{File};
+use std::io::{Read, Write};
 use std::path::Path;
 use std::string::FromUtf8Error;
 use crypto::{symmetriccipher, buffer, aes, blockmodes };
 use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
-use inquire::{CustomType, InquireError, required, Select, Text};
+use inquire::{CustomType, InquireError, Select};
 use inquire::validator::Validation;
 use console::Term;
 use miniz_oxide::deflate::compress_to_vec;
-use miniz_oxide::inflate::decompress_to_vec_with_limit;
+use miniz_oxide::inflate::decompress_to_vec;
 use crate::structurs::FileStruct;
+use console::style;
+
 
 fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
     let mut encryptor = aes::cbc_encryptor(
@@ -66,7 +68,11 @@ fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symm
 fn bytes_to_string(bytes: Vec::<u8>) -> Result<String, FromUtf8Error> {
     String::from_utf8(bytes)
 }
-
+fn pause() {
+    let term = Term::stdout();
+    println!("Press any key to continue...\r");
+    let a = term.read_char();
+}
 fn main() {
     let term = Term::stdout();
     term.set_title("File decryptor and cryptor");
@@ -154,34 +160,35 @@ fn main() {
                                             match ans {
                                                 Ok(choice) => {
                                                     if choice == "Decrypt" {
-                                                        let decompressed = decompress_to_vec_with_limit(buffer.as_slice(), usize::MAX).expect("Failed to decompress!");
-                                                        let decrypted_data = decrypt(decompressed.as_slice(), &key, &iv).ok().unwrap();
-
-                                                        let json = bytes_to_string(decrypted_data);
-                                                        match json {
-                                                            Ok(json) => {
-                                                                let file_struct = serde_json::from_str::<FileStruct>(json.as_str());
+                                                        let decompressed = decompress_to_vec(buffer.as_slice()).expect("Failed to decompress!");
+                                                        let decrypted_data = decrypt(decompressed.as_slice(), &key, &iv);
+                                                        match decrypted_data {
+                                                            Ok(decrypted_data) => {
+                                                                let json = bytes_to_string(decrypted_data);
+                                                                match json {
+                                                                    Ok(json) => {
+                                                                        let file_struct = serde_json::from_str::<FileStruct>(json.as_str());
 //
-                                                                match file_struct {
-                                                                    Ok(mut FileStruct) => {
-                                                                        println!("{}", extension.file_stem().unwrap().to_str().unwrap());
-                                                                        let mut f = File::create(format!("{}.{}",  extension.file_stem().unwrap().to_str().unwrap(), FileStruct.format));
-                                                                        match f {
-                                                                            Ok(mut file) => {
-                                                                                let decompressed = decompress_to_vec_with_limit(FileStruct.file.as_slice(), usize::MAX).expect("Failed to decompress!");
-                                                                                file.write_all(decompressed.as_slice()).expect("Error writing encrypted file");
+                                                                        match file_struct {
+                                                                            Ok(mut FileStruct) => {
+                                                                                println!("{}", extension.file_stem().unwrap().to_str().unwrap());
+                                                                                let mut f = File::create(format!("{}.{}",  extension.file_stem().unwrap().to_str().unwrap(), FileStruct.format));
+                                                                                match f {
+                                                                                    Ok(mut file) => {
+                                                                                        let decompressed = decompress_to_vec(FileStruct.file.as_slice()).expect("Failed to decompress!");
+                                                                                        file.write_all(decompressed.as_slice()).expect("Error writing encrypted file");
+                                                                                    }
+                                                                                    Err(_) => { println!("# Error creating file"); pause(); }
+                                                                                }
                                                                             }
-                                                                            Err(_) => println!("Error creating file"),
+                                                                            Err(_) => { println!("# Error reading file"); pause(); }
                                                                         }
+
                                                                     }
-                                                                    Err(_) => println!("Error reading file"),
-                                                                }
-
+                                                                    Err(_) => { println!("# Error reading file"); pause(); }                                                      }
                                                             }
-                                                            Err(_) => println!("Error reading file"),
+                                                            Err(_) => { println!("{}", style("# Wrong Key or IV").red()); pause(); }
                                                         }
-
-
                                                     }
                                                     else if choice == "Encrypt" {
                                                         let compressed = compress_to_vec(buffer.as_slice(), 6);
@@ -201,33 +208,31 @@ fn main() {
                                                                     Ok(mut file) => {
                                                                         file.write_all(compressed.as_slice()).expect("Error writing encrypted file");
                                                                     }
-                                                                    Err(_) => println!("Error creating file"),
+                                                                    Err(_) => { println!("# Error creating file"); pause(); }
                                                                 }
                                                             }
-                                                            Err(_) => println!("Error creating file"),
+                                                            Err(_) => { println!("# Error creating file"); pause(); }
                                                         }
-
-
-
                                                     }
                                                 },
-                                                Err(_) => println!("There was an error, please try again"),
+                                                Err(_) => { println!("# There was an error, please try again"); pause(); }
                                             }
 
                                         }
-                                        Err(_) => println!("Failed to open file"),
+                                        Err(_) => { println!("# Failed to open file"); pause(); }
                                     }
                                 }
-                                Err(_) => println!("Failed to open file"),
+                                Err(_) => { println!("# Failed to open file"); pause(); }
                             }
                         },
-                        Err(_) => println!("Failed to read the key"),
+                        Err(_) => { println!("# Failed to read the key"); pause(); }
                     }
                 },
-                Err(_) => println!("Failed to read the key"),
+                Err(_) => { println!("# Failed to read the key"); pause(); }
             }
 
         },
-        Err(_) => println!("File not found"),
+        Err(_) =>  { println!("# File not found"); pause(); }
+
     }
 }
